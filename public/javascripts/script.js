124e59d62f74495c5bddc49d5d32cf0cdb96e271
awaisabir <awaisabirqureshi@gmail.com>
2017-03-30 00:36:02 -0400

    register & login click handler system

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,18 +1,78 @@
 $(document).ready( () => {
+/*  time   => get Date and string
+    site   => the button value
+    user   => grab from the username button
+    scheme => emoji always
+    mode   => what the user wants to do
+    event  => was it a success
+    data   => nobody cares
+  */
 
-  let test_btn = $('#test');
+  let user = "";
+  let site = "";
+  let scheme = "";
+  let mode = "";
+  let event = "";
+  let data = "";
 
-  test_btn.click( () => {
+  let username = "";
+
+  let register_btn = $('#register');
+
+  register_btn.click(() => {
+
     let currentdate = new Date();
-    let datetime = currentdate.getFullYear() + "-" +
-                   (currentdate.getMonth()+1) + "-" +
-                   currentdate.getDate() + " " +
-                   currentdate.getHours() + ":" +
-                   currentdate.getMinutes();
-    let message = "Button Clicked\n";
+    time = currentdate.getFullYear() + "-" +
+                      (currentdate.getMonth()+1) + "-" +
+                      currentdate.getDate() + " " +
+                      currentdate.getHours() + ":" +
+                      currentdate.getMinutes();
+    site = "main";
+    user = $('#username').val();
+    scheme = "unknown"
+    mode = register_btn.html().toLowerCase();
+    event = "";
+    data = "";
 
-    $.post('/csv', {time: datetime, message: message}, (data) => {
-      console.log(data);
-    })
-  })
-})
+    if (user === "") {
+      noty({text: 'Enter a username!'});
+      event = "failure"
+      $.post('/csv', {time: time, site: site, user: user, scheme: scheme, mode: mode, event:event, data:data});
+    } else {
+
+      event = 'success';
+      $.post('/csv', {time: time, site: site, user: user, scheme: scheme, mode: mode, event:event, data:data});
+      username = user;
+      $('#username').val('');
+      register_btn.hide();
+      $('.username-wrapper').append('<button type="button" class="btn btn-default" id="login">Login</button>');
+    }
+
+    let login_btn = $('#login');
+
+    login_btn.click(() => {
+      let currentdate = new Date();
+      time = currentdate.getFullYear() + "-" +
+                        (currentdate.getMonth()+1) + "-" +
+                        currentdate.getDate() + " " +
+                        currentdate.getHours() + ":" +
+                        currentdate.getMinutes();
+
+      mode = login_btn.html().toLowerCase();
+      data = "";
+
+
+      let login_username = $('#username').val();
+      if(login_username === username) {
+        // display all the login stuff
+        $.post('/csv', {time: time, site: site, user: username, scheme: scheme, mode: mode, event:'success', data:data});
+      }
+      else {
+        noty({text: 'Username Incorrect', type: 'error'});
+        $.post('/csv', {time: time, site: site, user: username, scheme: scheme, mode: mode, event:'failure', data:data});
+      }
+    });
+
+  });
+
+});
