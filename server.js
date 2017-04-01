82fc29600c40c14de9debdc6cb2d3d17964e76db
awaisabir <awaisabirqureshi@gmail.com>
2017-03-31 20:10:19 -0400

    Added logic to append

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+/*
+  
+*/
+
+
 const express = require('express');
 const fs = require('fs');
 const emojiData = require('emoji-data');
 const bodyParser = require('body-parser');
 const json2csv = require('json2csv');
+let newLine = "\r\n";
 
 let app = express();
 
@@ -10,6 +16,15 @@ app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 let arr = [];
+let fields = [
+    'time',
+    'site',
+    'user',
+    'scheme',
+    'mode',
+    'event',
+    'data'
+];
 
 app.use(express.static(__dirname + '/public'));
 
@@ -22,47 +37,69 @@ app.listen(3000, () => {
 // default route
 app.get('/', (req, res, next) => {
 
-  res.sendFile(__dirname + '/public/html/index.html');
+    res.sendFile(__dirname + '/public/html/index.html');
 })
 
 // post endpoint for generating a csv
 app.post('/csv', (req, res, next) => {
 
-  let item = {
-    time:   req.body.time,
-    site:   req.body.site,
-    user:   req.body.user,
-    scheme: req.body.scheme,
-    mode:   req.body.mode,
-    event:  req.body.event,
-    data:   req.body.data
-};
-  arr.push(item);
+    let item = {
+        time: req.body.time,
+        site: req.body.site,
+        user: req.body.user,
+        scheme: req.body.scheme,
+        mode: req.body.mode,
+        event: req.body.event,
+        data: req.body.data
+    };
 
-  let csv = json2csv({data: arr});
-  fs.writeFile('./public/csv/file.csv', csv, (err) => {
-    if(err)
-      throw err;
+    fs.stat('./public/csv/file.csv', function(err, stat) {
+        if (err === null) {
 
-    console.log('file saved');
-  })
+            let toCsv = {
+                data: item,
+                fields: fields,
+                hasCSVColumnTitle: false
+            };
 
-  return res.json(req.body);
+            //write the actual data and end with newline
+            let csv = json2csv(toCsv) + newLine;
+
+            fs.appendFile('./public/csv/file.csv', csv, function(err) {
+                if (err)
+                    throw err;
+            });
+        } else {
+            //write the headers, data & newline
+            let toCsv = {
+                data: item,
+                fields: fields,
+                hasCSVColumnTitle: true
+            };
+            // fields= (fields + newLine);
+            let csv = json2csv(toCsv) + newLine;
+
+            fs.writeFile('./public/csv/file.csv', csv, function(err, stat) {
+                if (err)
+                    throw err;
+                }
+            );
+        }
+    });
 })
-
 
 // Added an endpoint to return all emojis
 app.get('/28_emojis', (req, res, next) => {
 
-  // emoji array
-  let emoji_array = [];
+    // emoji array
+    let emoji_array = [];
 
-  // push each JSON with the attributes I want
-  for (let i = 0; i < emojiData.all().length; i++) {
-      let proxyJSON = {};
-      proxyJSON[emojiData.all()[i].name] = emojiData.all()[i].render();
-      emoji_array.push(proxyJSON);
-  }
+    // push each JSON with the attributes I want
+    for (let i = 0; i < emojiData.all().length; i++) {
+        let proxyJSON = {};
+        proxyJSON[emojiData.all()[i].name] = emojiData.all()[i].render();
+        emoji_array.push(proxyJSON);
+    }
 
     // make a result array => 28
     let result_array = [];
